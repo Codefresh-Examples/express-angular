15e1fe5b031f0663937d105eba3ca4e07f966a01
grunzwei-fresh <nathan@codefresh.io>
2015-03-18 15:28:04 +0000

    change port to 9000 to work with codefresh defaults

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // set up ======================================================================
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
-var port  	 = process.env.PORT || 9090; 				// set the port
+var port  	 = process.env.PORT || 9000; 				// set the port
 
 var morgan = require('morgan'); 		// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
